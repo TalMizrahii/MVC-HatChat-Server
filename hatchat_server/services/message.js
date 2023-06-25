e79a15f98fe5ac48cbb6cca08d38f6a796eb9093
Yuval Arbel <Arbelyuval56@gmail.com>
2023-06-26 01:52:33 +0300

    android server finish

--- hatchat_server/services/message.js
+++ hatchat_server/services/message.js
@@ -16,55 +16,66 @@ const sendMessage = async (username, fullMsg) => {
 }
 
 const sendMessageAndroid = async (username, fullMsg) => {
-    for (let i = 0; i < fullMsg.content.length; i += 2500) {
-        const msg = {
-            notification: {
-                title: 'Message from ' + fullMsg.sender.username.toString(),
-                body: fullMsg.content.substring(i, i + 2500),
-            },
-            data: {
-                senderUsername: fullMsg.sender.username.toString(),
-                senderDisplayName: fullMsg.sender.displayName.toString(),
-                senderProfilePic: fullMsg.sender.profilePic.toString(),
-                created: fullMsg.created.toISOString(),
-                chatID: fullMsg.id.toString(),
-                content: fullMsg.content.toString(),
-            },
-            token: androidTokensArray[username]
-        };
-        await firebaseAdmin.messaging.send(msg)
-            .then((response) => {
-                console.log('Message sent!');
-            }).catch((err) => {
-                console.log('Sending message failed: ', err);
+
+    const msg = {
+        notification: {
+            title: 'Message from ' + fullMsg.sender.username.toString(),
+            body: fullMsg.content.substring(0, 4096),
+        },
+        data: {
+            senderUsername: fullMsg.sender.username.toString(),
+            senderDisplayName: fullMsg.sender.displayName.toString(),
+            senderProfilePic: fullMsg.sender.profilePic.toString(),
+            created: fullMsg.created.toISOString(),
+            chatID: fullMsg.id.toString(),
+            content: fullMsg.content.toString(),
+        },
+        token: androidTokensArray[username]
+    };
+    await firebaseAdmin.messaging.send(msg)
+        .then((response) => {
+            console.log('Message sent!');
+        }).catch((err) => {
+            console.log('Sending message failed: ', err);
+        });
+}
+
+const addMessage = async (id, content, connectUsername) => {
+    try {
+        const chat = await Chat.findOne({"id": id});
+
+        if (chat) {
+            const user0 = await User.findOne({"_id": chat.users[0]}).lean();
+            const user1 = await User.findOne({"_id": chat.users[1]}).lean();
+            if (!await chatService.chatValidation(await user0.username, await user1.username, connectUsername)) {
+                return false;
+            }
+            const sender = await User.findOne({"username": connectUsername})
+            const maxMessageID = await Message.findOne().sort('-id').limit(1).exec();
+            let messageID = 1;
+            if (maxMessageID && maxMessageID.senderMessageCount) {
+                messageID = maxMessageID.senderMessageCount + 1;
+            }
+
+            const newMessage = await new Message({
+                "senderMessageCount": messageID,
+                "sender": sender,
+                "content": content
             });
-    }
-};
 
-    const addMessage = async (id, content, connectUsername) => {
-        try {
-            const chat = await Chat.findOne({"id": id});
-
-            if (chat) {
-                const user0 = await User.findOne({"_id": chat.users[0]}).lean();
-                const user1 = await User.findOne({"_id": chat.users[1]}).lean();
-                if (!await chatService.chatValidation(await user0.username, await user1.username, connectUsername)) {
-                    return false;
-                }
-                const sender = await User.findOne({"username": connectUsername})
-                const maxMessageID = await Message.findOne().sort('-id').limit(1).exec();
-                let messageID = 1;
-                if (maxMessageID && maxMessageID.senderMessageCount) {
-                    messageID = maxMessageID.senderMessageCount + 1;
-                }
-
-                const newMessage = await new Message({
-                    "senderMessageCount": messageID,
-                    "sender": sender,
-                    "content": content
-                });
-
-                const returnVal = {
+            const returnVal = {
+                "id": messageID,
+                "created": new Date(),
+                "sender": {
+                    "username": sender.username,
+                    "displayName": sender.displayName,
+                    "profilePic": sender.profilePic
+                },
+                "content": content
+            }
+            const fullMsg = {
+                "id": id,
+                "message": {
                     "id": messageID,
                     "created": new Date(),
                     "sender": {
@@ -74,77 +85,67 @@ const sendMessageAndroid = async (username, fullMsg) => {
                     },
                     "content": content
                 }
-                const fullMsg = {
-                    "id": id,
-                    "message": {
-                        "id": messageID,
-                        "created": new Date(),
-                        "sender": {
-                            "username": sender.username,
-                            "displayName": sender.displayName,
-                            "profilePic": sender.profilePic
-                        },
-                        "content": content
-                    }
-                }
+            }
 
-                if (androidTokensArray[user0.username] === connectUsername) {
+            if (androidTokensArray[user1.username]) {
+                if (user0.username === connectUsername) {
                     await sendMessageAndroid(user1.username, fullMsg);
-                } else if (androidTokensArray[user1.username] === connectUsername) {
-                    await sendMessageAndroid(user0.username, fullMsg);
+                }
+            } else if (androidTokensArray[user0.username]) {
+                await sendMessageAndroid(user0.username, fullMsg);
+            } else {
+                if (user0.username === connectUsername) {
+                    await sendMessage(user1.username, fullMsg);
                 } else {
-                    if (user0.username === connectUsername) {
-                        await sendMessage(user1.username, fullMsg);
-                    } else {
-                        await sendMessage(user0.username, fullMsg);
-                    }
+                    await sendMessage(user0.username, fullMsg);
                 }
-
-
-                await newMessage.save();
-                chat.messages.push(newMessage);
-                await chat.save();
-                return returnVal;
             }
-            return false;
-        } catch (err) {
-            console.log(err);
-            return false;
+
+
+            await newMessage.save();
+            chat.messages.push(newMessage);
+            await chat.save();
+            return returnVal;
         }
-    };
+        return false;
+    } catch (err) {
+        console.log(err);
+        return false;
+    }
+};
 
 
-    const getMessages = async (id, connectUsername) => {
-        try {
-            const chatArray = []
-            const chat = await Chat.findOne({"id": id});
-            if (chat) {
-                const user0 = await User.findOne({"_id": chat.users[0]}).lean();
-                const user1 = await User.findOne({"_id": chat.users[1]}).lean();
-                if (!await chatService.chatValidation(await user0.username, await user1.username, connectUsername)) {
-                    return false;
-                }
-                for (const msg of chat.messages) {
-                    const getMsg = await Message.findOne({"_id": msg});
-                    const sender = await User.findOne(msg.sender).populate('username')
-                    chatArray.push({
-                        "id": getMsg.senderMessageCount,
-                        "created": getMsg.created,
-                        "sender": {
-                            "username": sender.username
-                        },
-                        "content": getMsg.content
-                    });
-                }
-                return chatArray;
+const getMessages = async (id, connectUsername) => {
+    try {
+        const chatArray = []
+        const chat = await Chat.findOne({"id": id});
+        if (chat) {
+            const user0 = await User.findOne({"_id": chat.users[0]}).lean();
+            const user1 = await User.findOne({"_id": chat.users[1]}).lean();
+            if (!await chatService.chatValidation(await user0.username, await user1.username, connectUsername)) {
+                return false;
             }
-            return false;
-        } catch (err) {
-            console.log(err);
-            return false;
+            for (const msg of chat.messages) {
+                const getMsg = await Message.findOne({"_id": msg});
+                const sender = await User.findOne(msg.sender).populate('username')
+                chatArray.push({
+                    "id": getMsg.senderMessageCount,
+                    "created": getMsg.created,
+                    "sender": {
+                        "username": sender.username
+                    },
+                    "content": getMsg.content
+                });
+            }
+            return chatArray;
         }
+        return false;
+    } catch (err) {
+        console.log(err);
+        return false;
+    }
 
 
-    };
+};
 
-    export default {addMessage, getMessages}
+export default {addMessage, getMessages}
